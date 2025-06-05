9fbe78957224620b41a0f188bd98671a539435ea
Gaelle Oxine <gaelle@Gaelles-MacBook-Air.local>
2025-06-04 22:53:42 -0400

    Added Generale Styles, added style for mobile. Fixed grid element overflowing conatainer.

--- calculator.js
+++ calculator.js
@@ -1,71 +1,74 @@
+const display = document.querySelector(".display p");
+
 function getInput(input) {
-    const display = document.querySelector(".display");
-    if (display.textContent.length == 0 && "+-*/".includes(input)) {
-      display.textContent = "0" + input;
-    } else {
-        display.textContent += input;
-    }
+  if (display.textContent.length == 0 && "+-*/".includes(input)) {
+    display.textContent = "0" + input;
+  } else {
+    display.textContent += input;
+  }
 }
 
 function clearDisplay() {
-    const display = document.querySelector(".display");
-    display.textContent = "";
-
+  display.textContent = "";
 }
 
 function backSpace() {
-    const display = document.querySelector(".display");
-    if (display.textContent.length > 0) {
-      displayedText = display.textContent.slice(0, -1);
-      display.textContent = displayedText;
-    }
-    
+  if (display.textContent.length > 0) {
+    displayedText = display.textContent.slice(0, -1);
+    display.textContent = displayedText;
+  }
 }
 function operate() {
-    const display = document.querySelector(".display");
-    displayedText = display.textContent;
-    const inputList = displayedText.split(/([+\-*/])/);
-    let result = parseFloat(inputList[0]);
+  displayedText = display.textContent;
+  const inputList = displayedText.split(/([+\-*/])/);
+  let result = parseFloat(inputList[0]);
 
-    for (let i = 1; i < inputList.length; i+=2){
-        const operator = inputList[i]
-        const operand = parseFloat(inputList[i + 1])
+  for (let i = 1; i < inputList.length; i += 2) {
+    const operator = inputList[i];
+    const operand = parseFloat(inputList[i + 1]);
 
-        if (operator == "+") {result += operand};
-        if (operator == "-") {result -= operand};
-        if (operator == "*") {result *= operand};
-        if (operator == "/") {result /= operand};
+    if (operator == "+") {
+      result += operand;
     }
-    return result
- 
+    if (operator == "-") {
+      result -= operand;
+    }
+    if (operator == "*") {
+      result *= operand;
+    }
+    if (operator == "/") {
+      result /= operand;
+    }
+  }
+  return result;
 }
 
-function displayResult (result) {
-    const display = document.querySelector(".display");
-    display.textContent = result;
-    
+function displayResult(result) {
+  display.textContent = result;
 }
 
 function orchestrate() {
-    const numbersSignsButtons = document.querySelectorAll(".numbers, .signs, .decimalpoint");
-    const clearButton = document.querySelector(".clear");
-    const backSpaceButton = document.querySelector(".delete");
-    const equalSign = document.querySelector(".equal")
-   
-    for (let btn of numbersSignsButtons) {
-      btn.addEventListener("click", (event) => {
-        getInput(btn.textContent);
-      });
-    }
+  const numbersSignsButtons = document.querySelectorAll(
+    ".numbers, .signs, .decimalpoint"
+  );
+  const clearButton = document.querySelector(".clear");
+  const backSpaceButton = document.querySelector(".delete");
+  const equalSign = document.querySelector(".equal");
 
-    clearButton.addEventListener("click", clearDisplay);
+  for (let btn of numbersSignsButtons) {
+    btn.addEventListener("click", (event) => {
+      getInput(btn.textContent);
+    });
+  }
 
-    backSpaceButton.addEventListener("click", backSpace);
+  clearButton.addEventListener("click", clearDisplay);
 
-    equalSign.addEventListener("click", () => {
-        const result = operate();
-        displayResult(result);
-    })
+  backSpaceButton.addEventListener("click", backSpace);
+
+  equalSign.addEventListener("click", () => {
+    const result = operate();
+    displayResult(result);
+  });
 }
 
-orchestrate()
+orchestrate();
